91b3ab56a46783a91a5d43842c933197bae9ef22
shravyasrigande <150170807+shravyasrigande@users.noreply.github.com>
2023-11-21 23:20:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+*{
+    background-color:black;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+* input,label{
+    color: white;
+}
+#thofuu{
+    line-height: 40px;
+}
+#thofuu{
+    padding-top: 80px;
+    padding-left: 700px;
+    font-size: medium;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    width: 500px;
+    height: 400px;
+
+}
+input{
+    padding-left: 30px;
+}
+#name label{
+    padding-left: 50px;
+}
+#data{
+    padding-top: none;
+    padding-left: 450px;
+    font-size: medium;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    height: 400px;
+    color: white;
+}
+#data td{
+    padding-left: 20px;
+}
+#thofuu label{
+    padding-right:40px;
+}
+button{
+    color: white;
+    background-color: green;
+    border-radius: 15px;
+    font-size: medium;
+    padding-left: 25px;
+    padding-right: 25px;
+}
+h2{
+    color: white;
+}
+button:hover{
+    color: white;
+    background-color: black;
+}
